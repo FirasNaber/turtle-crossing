74ad71aecba679958384ea2c39aa0d842e068d65
Firas Naber <firasnaber@gmail.com>
2022-09-26 00:52:32 -0500

    Edit car manager class

--- car_manager.py
+++ car_manager.py
@@ -1,22 +1,33 @@
-from player import FINISH_LINE_Y
 from random import choice, randint
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+current_moving_distance = STARTING_MOVE_DISTANCE
 
 
-class CarManager(Turtle):
+class CarManager:
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.color(choice(COLORS))
-        self.shapesize(stretch_len=2)
-        self.penup()
-        starting_position = (300, randint(-280, FINISH_LINE_Y))
-        self.setposition(starting_position)
+        self.cars = []
+
+    def create(self):
+        random_chance = randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle(shape="square")
+            new_car.color(choice(COLORS))
+            new_car.shapesize(stretch_len=2)
+            new_car.penup()
+            starting_position = (300, randint(-260, 260))
+            new_car.setposition(starting_position)
+            self.cars.append(new_car)
 
     def move(self):
-        new_x = self.xcor() - STARTING_MOVE_DISTANCE
-        self.goto(x=new_x, y=self.ycor())
+        for car in self.cars:
+            car.backward(current_moving_distance)
+
+    def speed_up(self):
+        global current_moving_distance
+        current_moving_distance += MOVE_INCREMENT
+        for car in self.cars:
+            car.backward(STARTING_MOVE_DISTANCE + current_moving_distance)
